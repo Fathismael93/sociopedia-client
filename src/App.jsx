9c17b91e4f6fa15c58caeaba294321757dc0924d
Fathi Ahmed <fathismael@gmail.com>
2023-09-21 08:23:00 +0300

    changes with imports made

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "./scenes/homePage";
-import LoginPage from "./scenes/LoginPage";
+import LoginPage from "./scenes/loginPage";
 import ProfilePage from "./scenes/ProfilPage";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
